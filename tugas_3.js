6617064523dde2ef25937f56bedc06eaa5faae62
Gendrenk <nasrullohfath@gmail.com>
2022-11-15 10:26:35 +0700

    Tugas 3 JS

---
+++ tugas_3.js
@@ -0,0 +1,8 @@
+var namaDepan = "Andi";
+function namaBelakang(nama){
+    var namaLengkap = nama + "Samsul";
+    return namaLengkap
+}
+
+console.log(namaDepan)
+console.log(namaBelakang(namaDepan))
